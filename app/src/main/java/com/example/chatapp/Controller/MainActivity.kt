3fb81d1ff1f1bd74f337955be6db86b5b3bbc2bc
Nishal <nishaldevadiga2003@gmail.com>
2023-02-13 22:45:47 +0530

    format

--- app/src/main/java/com/example/chatapp/Controller/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/Controller/MainActivity.kt
@@ -22,11 +22,15 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
     }
-
 
 
 }
